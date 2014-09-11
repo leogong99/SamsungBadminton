3be02d5c009de02582ce32e3ecf6e88874eb710d
jing gong <leogong99@gmail.com>
2014-09-10 23:30:01 -0700

    database setup

--- dist/server/config/environment/development.js
+++ dist/server/config/environment/development.js
@@ -3,10 +3,10 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/samsungbadminton-dev'
-  },
+	// MongoDB connection options
+	mongo: {
+		uri: 'mongodb://heroku_app29451703:vqgdlta54mft988jd1kipck4im@ds035310.mongolab.com:35310/heroku_app29451703'
+	},
 
-  seedDB: true
+	seedDB: true
 };
