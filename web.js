0dd844c6e47c9fc7d653bb78acdb28d7c57709ba
jing gong <leogong99@gmail.com>
2014-09-10 20:59:26 -0700

    Remove logger.

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express(); 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/dist/public"));
 app.listen(process.env.PORT || 9000);
