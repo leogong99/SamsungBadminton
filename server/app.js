b5ddd023eb9d71f892833f4888ed3a1a4b449dea
jing gong <leogong99@gmail.com>
2014-09-10 22:56:08 -0700

    more ico fix

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ if (config.seedDB) {
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
+app.use(favicon(__dirname + '/public/favicon.ico'));
 require('./config/express')(app);
 require('./routes')(app);
 
